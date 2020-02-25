40c4f83f73474eb710ce6ba3e88b71dc4800162d
Oagile Segobaetso <osegobae@c4r9s4.wethinkcode.co.za>
2020-02-25 13:22:37 +0200

    fixed strcmp to work on unsigned char *

--- ex00_d06_lib/ft_strcmp.c
+++ ex00_d06_lib/ft_strcmp.c
@@ -6,24 +6,27 @@
 /*   By: osegobae <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 13:25:49 by osegobae          #+#    #+#             */
-/*   Updated: 2020/02/18 13:56:35 by osegobae         ###   ########.fr       */
+/*   Updated: 2020/02/25 13:19:25 by osegobae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strcmp(char *s1, char *s2)
+int		ft_strcmp(char *s1, char *s2)
 {
-	int len;
-	int i;
+	int				i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
-	len = 0;
 	i = 0;
-	while (s1[len] != '\0')
-	{
-		++len;
-	}
-	while (s2[i] != '\0')
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (str1[i] && str[i] && str1[i] == str2[i])
 	{
 		++i;
 	}
-	return (len - i);
+	if ((str1 - str2) < 0)
+		return (-1);
+	else if ((str1 - str2) > 0)
+		return (1);
+	else
+		return (0);
 }
