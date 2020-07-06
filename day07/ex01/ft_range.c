0593f5880641ccca34413ac0d88a9b3e92a5dc8a
Bankslado <oagilesegobaetso3@gmail.com>
2020-07-06 02:16:51 +0200

    adding day07/ex01

---
+++ day07/ex01/ft_range.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int *ints_arr;
+	int i;
+
+	if (min >= max)
+		return (0);
+	ints_arr = (int *)malloc(sizeof(int) * (max - min));
+	if (ints_arr == NULL)
+		return (ints_arr);
+	i = 0;
+	while (min < max)
+	{
+		ints_arr[i] = min;
+		++min;
+		++i;
+	}
+	return (ints_arr);
+}
