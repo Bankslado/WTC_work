4c0852d5b49ba62525af6f5d809591d60ba1ccd2
Bankslado <oagilesegobaetso3@gmail.com>
2020-07-06 02:17:57 +0200

    Added day07/ex02

---
+++ day07/ex02/ft_ultimate_range.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int i;
+
+	i = 0;
+	if (min >= max)
+		return (0);
+	*range = (int *)malloc(sizeof(int) * (max - min));
+	if (*range == NULL)
+		return (0);
+	while (min < max)
+	{
+		*(*range + i) = min;
+		++min;
+		++i;
+	}
+	return (i);
+}
